16923413702487ef84d879de64709fef8ffeb9c8
YaserObeid <yaser.obeid@hotmail.com>
2022-02-11 16:47:15 +0100

    full crud rest

--- src/main/java/com/obeid/springrest/crud/rest/CustomerRestController.java
+++ src/main/java/com/obeid/springrest/crud/rest/CustomerRestController.java
@@ -2,7 +2,12 @@ package com.obeid.springrest.crud.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +28,48 @@ public class CustomerRestController {
 	public List<Customer> getCustomers(){
 		
 		return  customerService.getCustomers();
+	}
+	
+	// read customer by id Get: /customer/{customerId}
+	
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		
+		Customer customer = customerService.getCustomer(customerId);
+		return customer;
+	}
+	
+	// add a new customer POST: /customers
+	
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer customer) {
+		// if id == null -> add , else update
+		// to force add new customer: id = 0 (null)
+		customer.setId(0); 
+		customerService.saveCustomer(customer);
+		
+		return customer;
+	}
+	
+	// update a given customer PUT: /customer
+	
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer customer) {
+		// since the given customer has id -> id != null
+		// -> apply update
+		customerService.saveCustomer(customer);
+		
+		return customer;
+	}
+	
+	// delete a customer by id DELETE: /customers/{cusomerId}
+	
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
+		
+		customerService.deleteCustomer(customerId);
+		
+		return "customer with id: " + customerId + " is deleted";
 	}
 	
 	
